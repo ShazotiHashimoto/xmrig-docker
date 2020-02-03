5328050b2a51c635a3fffdfbb6a85646cdb1e3c5
shahzaad <shahzaadt@gmail.com>
2020-02-03 22:28:28 +0000

    scriptupdates

--- PS Scripts/westeurope.ps1
+++ PS Scripts/westeurope.ps1
@@ -1,6 +1,6 @@
 #az account set --subscription 10b9ff10-0535-4ed0-b2cb-be085edb855a
-$i=0
-while($i -lt 101)
+$i=999
+while($i -lt 1040)
 {
     $i++
 	az container create -g main --name westeuropemachine0$i --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 4  --location westeurope
