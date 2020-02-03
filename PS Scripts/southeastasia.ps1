5e87a7cc429fcf1fb99fee59b940037bf983c219
shahzaadt <shahzaadt@gmail.com>
2020-02-03 10:57:42 +0000

    updated for stone

--- PS Scripts/southeastasia.ps1
+++ PS Scripts/southeastasia.ps1
@@ -1,9 +1,9 @@
 #az account set --subscription 10b9ff10-0535-4ed0-b2cb-be085edb855a
-$i=9999999
+$i=0999
 while($i -lt 101)
 {
     $i++
-	az container create -g main --name southeastasiamachine0$i --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 4  --location southeastasia
+	az container create -g main --name southeastasiamachine0$i --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 4  --location southeastasia
 }
 
 
@@ -36,28 +36,28 @@ while($i -lt 101)
 
 
 
-az container create -g mineroot --name southeastasiadockermine1 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine2 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine3 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine4 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine5 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine6 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine7 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine8 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine9 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine10 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine11 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine12 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine13 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine14 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine15 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine16 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine17 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine18 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine19 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine20 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine21 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine22 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine23 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine24 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
-az container create -g mineroot --name southeastasiadockermine25 --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine1 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine2 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine3 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine4 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine5 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine6 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine7 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine8 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine9 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine10 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine11 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine12 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine13 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine14 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine15 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine16 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine17 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine18 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine19 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine20 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine21 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine22 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine23 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine24 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
+az container create -g mineroot --name southeastasiadockermine25 --image milennialsafezone/xmrig_stone:v2 --os-type Linux --cpu 4 --memory 8  --location southeastasia;
