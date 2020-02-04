8c65c94fd3bf4f32d8bc8f3e9ab08352e7133371
Shahzaad Tayob <shahzaadt@gmail.com>
2020-02-04 09:34:55 +0200

    updates us ps

---
+++ PS Scripts/centralus.ps1
@@ -0,0 +1,8 @@
+#az account set --subscription 4c28e054-4477-4dae-bf76-478ca163cd80
+$i=0
+$RANDOM = Get-Random
+while($i -lt 101)
+{
+    $i++
+	az container create -g main --name centralusmachine0$i$RANDOM --image shahzaadt/xmrig:v2 --os-type Linux --cpu 4 --memory 4  --location centralus
+}
