900f6a057b8c6c667e0a0adb1b3e6c149098fe20
shahzaad <shahzaadt@gmail.com>
2020-02-04 06:22:19 +0000

    fix random number

--- PS Scripts/northeurope.ps1
+++ PS Scripts/northeurope.ps1
@@ -1,5 +1,6 @@
 #az account set --subscription 4c28e054-4477-4dae-bf76-478ca163cd80
 $i=0
+$RANDOM = Get-Random
 while($i -lt 101)
 {
     $i++
--- PS Scripts/westeurope.ps1
+++ PS Scripts/westeurope.ps1
@@ -1,5 +1,6 @@
 #az account set --subscription 10b9ff10-0535-4ed0-b2cb-be085edb855a
 $i=0
+$RANDOM = Get-Random
 while($i -lt 1040)
 {
     $i++
